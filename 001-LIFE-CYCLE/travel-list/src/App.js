38094401b1f273541c16d0d5e2f542d3085b2a86
Firat Atalay <atafiratbey@gmail.com>
2024-02-12 23:02:35 +0300

    Over and over until I am gonna finish this course

--- 001-LIFE-CYCLE/travel-list/src/App.js
+++ 001-LIFE-CYCLE/travel-list/src/App.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+// ];
 
 export default function App() {
   const [items, setItems] = useState([]);
