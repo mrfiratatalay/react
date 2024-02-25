fa0c2226a2fdf5926d9d364a8533985daa017f8e
mrfiratatalay <atafiratbey@gmail.com>
2024-02-25 16:52:38 +0300

    New Day New Project

---
+++ 002-LIFE-CYCLE/todayilearned/00/App.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+export default function App() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
+}
