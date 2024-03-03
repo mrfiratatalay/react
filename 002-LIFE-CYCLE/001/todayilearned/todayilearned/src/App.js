6600d48586c54149569829cbac2f1eed760a2307
mrfiratatalay <atafiratbey@gmail.com>
2024-03-03 12:06:15 +0300

    Adding a New Fact – Part 2

--- 002-LIFE-CYCLE/001/todayilearned/todayilearned/src/App.js
+++ 002-LIFE-CYCLE/001/todayilearned/todayilearned/src/App.js
@@ -37,15 +37,18 @@ const initialFacts = [
 
 export default function App() {
   const [showForm, setShowForm] = useState(false);
+  const [facts, setFacts] = useState(initialFacts);
 
   return (
     <>
       <Header showForm={showForm} setShowForm={setShowForm} />
-      {showForm ? <NewFactForm /> : null}
+      {showForm ? (
+        <NewFactForm setFacts={setFacts} setShowForm={setShowForm} />
+      ) : null}
 
       <main className="main">
         <CategoryFilter />
-        <FactList />
+        <FactList facts={facts} />
       </main>
     </>
   );
@@ -89,19 +92,19 @@ function isValidHttpUrl(string) {
   }
   return url.protocol === "http:" || url.protocol === "https:";
 }
-function NewFactForm() {
+function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState("");
   const [source, setSource] = useState("");
   const [category, setCategory] = useState("");
   const textLength = text.length;
 
   function handleSubmit(e) {
-    //Prevent browser reload
+    // 1. Prevent browser reload
     e.preventDefault();
 
-    //Check if data is valid. If so, create new fact
+    // 2. Check if data is valid. If so, create new fact
     if (text && isValidHttpUrl(source) && category && textLength <= 200) {
-      //Create a new fact form
+      // 3. Create a new fact form
       const newFact = {
         id: Math.round(Math.random() * 10000000),
         text,
@@ -110,8 +113,19 @@ function NewFactForm() {
         votesInteresting: 0,
         votesMindblowing: 0,
         votesFalse: 0,
-        createdIn: new Date().getCurrentYear(),
+        createdIn: new Date().getFullYear(),
       };
+
+      // 4. Add the new fact to the UI: add the fact to state
+      setFacts((facts) => [newFact, ...facts]);
+
+      // 5. Reset input fields
+      setText("");
+      setSource("");
+      setCategory("");
+
+      // 6. Close the form
+      setShowForm(false);
     }
   }
 
@@ -177,9 +191,7 @@ function CategoryFilter() {
   );
 }
 
-function FactList() {
-  const facts = initialFacts;
-
+function FactList({ facts }) {
   return (
     <section>
       <ul>
